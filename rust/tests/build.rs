b5a0f9455333ab65a531a9cbb4b751ded3a77487
Peter Tripp <petertripp@gmail.com>
2026-08-26 15:18:43 -0400

    And stuff

--- rust/tests/build.rs
+++ rust/tests/build.rs
@@ -1,5 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().unwrap());
+    if std::env::var_os("PROTOC").is_none() {
+        if let Ok(protoc) = protoc_bin_vendored::protoc_bin_path() {
+            unsafe {
+                std::env::set_var("PROTOC", protoc);
+            }
+        }
+    }
     tonic_prost_build::configure()
         .build_server(true)
         .build_client(false)
